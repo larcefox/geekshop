0cfd58d0d9b176580fcc7a5cb6f3c19ea6f01005
Larce Fox <larcethefox@gmail.com>
2020-05-16 03:15:00 +0300

    Lesson3 done

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,15 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", mainapp.main, name="main"),
     path("products/", mainapp.products, name="products"),
-    path("products/all", mainapp.products, name="products_all"),
-    path("products/home", mainapp.products, name="products_home"),
-    path("products/office", mainapp.products, name="products_office"),
-    path("products/modern", mainapp.products, name="products_modern"),
-    path("products/classic", mainapp.products, name="products_classic"),
     path("contact/", mainapp.contact, name="contact"),
+    path('products/', include('mainapp.urls', namespace='products'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
