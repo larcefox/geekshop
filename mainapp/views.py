2757f20b0b9e581f84bfbe673901d5a5bad5d5a7
Larce Fox <larcethefox@gmail.com>
2020-06-01 16:58:30 +0300

    Lesson 5 done

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
 from .models import Product, ProductCategory, Contact
+from basketapp.models import Basket
 
 
 
@@ -16,18 +18,44 @@ def main(request):
 
 
 def products(request, pk=None):
-    title = "продукты"
+    print(pk)
+
+    title = 'продукты'
     links_menu = ProductCategory.objects.all()
-    same_products = Product.objects.all()
-    content = {
-        "title": title,
-        "links_menu": links_menu,
-        "same_products": same_products,
-        "media_url": settings.MEDIA_URL,
-    }
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
     if pk:
-        print(f"User select category: {pk}")
-    return render(request, "mainapp/products.html", content)
+        if pk == '0':
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+            'basket': basket,
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
+
+
+    same_products = Product.objects.all()[3:5]
+
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
+    }
+
+    return render(request, 'mainapp/products.html', content)
+
 
 
 def contact(request):
